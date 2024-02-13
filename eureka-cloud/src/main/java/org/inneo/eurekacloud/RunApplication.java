ff062e1086f783fa822acf0e1974bc2ef3cdfc6e
Eduardo Cruz <tecnologiaesolucoespr@gmail.com>
2024-02-13 12:25:31 -0300

    Spring cloud eureka

---
+++ eureka-cloud/src/main/java/org/inneo/eurekacloud/RunApplication.java
@@ -0,0 +1,15 @@
+package org.inneo.eurekacloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunApplication.class, args);
+	}
+
+}
